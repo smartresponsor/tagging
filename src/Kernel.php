ede0dea872ed3593a6ed1296662f00ef6a75b1e8
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-04-12 23:18:50 -0500

    Add Symfony-native kernel baseline

---
+++ src/Kernel.php
@@ -0,0 +1,14 @@
+<?php
+
+// Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
+declare(strict_types=1);
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+final class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}

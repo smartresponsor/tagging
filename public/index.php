ab83620a183e2bdb37ece053303b734661912efa
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-04-14 00:00:01 -0500

    Fix Symfony-native cutover defects

--- public/index.php
+++ public/index.php
@@ -2,4 +2,21 @@
 
 declare(strict_types=1);
 
-require dirname(__DIR__).'/host-minimal/index.php';
+use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__) . '/config/bootstrap.php';
+
+if (($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? '1') && class_exists(Debug::class)) {
+    Debug::enable();
+}
+
+$kernel = new Kernel(
+    $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev',
+    (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? true),
+);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
